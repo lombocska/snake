bd791c03db358db4395d00efe53e9f11ba183cdd
Zsolt Tamás <tzsolt92@gmail.com>
2016-05-12 22:38:23 +0200

    Control system revised and GameOver screen appears when out of bounds

--- snake_moni.py
+++ snake_moni.py
@@ -6,6 +6,11 @@ import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
 
+def GameOver():
+    win.addstr(22, 70, "GAME OVER")
+    win.refresh()
+    time.sleep(3)
+
 # Choose mode
 mode = int(input(" \n" "Press button of F11! :]  \n" " \n" "Which mode do you want to play: \n" "1: If snake crosses the boundaries, make it enter from the other side \n" "2: Exit if snake crosses the boundaries \n" ))
 difficulty = int(input("\nOn which diffuculty would you like to play?\n1:Easy\n2:Medium\n3:Hard\n4:Insane\n"))
@@ -65,7 +70,17 @@ while key != 27:                                                   # While Esc k
 
     prevKey = key                                                  # Previous key pressed
     event = win.getch()
-    key = key if event == -1 else event
+    if event == KEY_DOWN and prevKey == KEY_UP:
+        key ==prevKey
+    elif event == KEY_UP and prevKey == KEY_DOWN:
+        key ==prevKey
+    elif event == KEY_LEFT and prevKey == KEY_RIGHT:
+        key ==prevKey
+    elif event == KEY_RIGHT and prevKey == KEY_LEFT:
+        key ==prevKey
+    elif event == -1:
+        key = key
+    else: key = event
 
     #Pause
     if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
@@ -78,6 +93,9 @@ while key != 27:                                                   # While Esc k
     #if the user is stupid
     if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
         key = prevKey
+
+
+
 
     # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
     # This is taken care of later at [1].
@@ -92,25 +110,21 @@ while key != 27:                                                   # While Esc k
 
     # Exit if snake crosses the boundaries
     if mode == 2:
-        if snake[0][0] == 0 or snake[0][0] == 44  or snake[0][1] == 0 or snake[0][1] == 149: break
+        if snake[0][0] == 0 or snake[0][0] == 44  or snake[0][1] == 0 or snake[0][1] == 149:
+            GameOver()
+            break
 
-    # If snake runs over itself
+    # Events of gameover
     if snake[0] in snake[1:]:
-        win.addstr(22, 70, "GAME OVER")
-        win.refresh()
-        time.sleep(2)
+        GameOver()
         break
 
     if (snake [0][1] == 80 and snake[0][0] <= 20):
-        win.addstr(22, 70, "GAME OVER")
-        win.refresh()
-        time.sleep(2)
+        GameOver()
         break
 
     if (snake[0][0] == 10 and snake[0][1] <= 20):
-        win.addstr(22, 70, "GAME OVER")
-        win.refresh()
-        time.sleep(2)
+        GameOver()
         break
 
 
@@ -120,7 +134,8 @@ while key != 27:                                                   # While Esc k
         while food1 == []:
             food1 = [randint(1, 43), randint(1, 148)]                 # Calculating next food's coordinates
             if food1 in snake: food1 = []
-            if food1 == 10 and food1 <= 20: food1 = []
+            if food1[0] == 10 and food1[1] <= 20: food1 = []
+            if food1[0] <= 40 and food1[1] == 20: food1 = []
         win.addch(food1[0], food1[1], '*')
     elif snake[0] == food2:                                            # When snake eats the food
         food2 = []

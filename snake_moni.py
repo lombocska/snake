0547e577ac77e21e3edf378c648bb72f7ed35cc4
Zsolt Tamás <tzsolt92@gmail.com>
2016-05-25 13:53:21 +0200

    cleaning code

--- snake_moni.py
+++ snake_moni.py
@@ -1,7 +1,5 @@
-# SNAKES GAME
+"""SNAKES GAME"""
 # Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for
-# exiting
-
 import curses
 import time
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
@@ -16,21 +14,17 @@ def GameOver():
 
 def appears_wall_vertical(x):
     for i in range(1, 21):
-        win.addstr(i, x, "|", curses.color_pair(4))
+        win.addstr(i, x, chr(176), curses.color_pair(4))
         win.refresh()
-
-
-x = randint(60, 80)
 
 
 def appears_wall_horizontal(y):
     for i in range(1, 21):
-        win.addstr(y, i, "-", curses.color_pair(4))
+        win.addstr(y, i, chr(1126), curses.color_pair(4))
         win.refresh()
 
-y = randint(10, 20)
 
-# Choose mode
+"""CHOOSE MODE"""
 mode = int(input(
     "\nPress button of F11! :]\n\nWhich mode do you want to play:"
     "\n1: If snake crosses the boundaries, make it enter from the other side"
@@ -46,7 +40,7 @@ elif difficulty == 3:
 elif difficulty == 4:
     speed = 15
 
-# default settings :)
+""" DEFAULT SETTINGS"""
 curses.initscr()
 curses.start_color()
 box = curses.newwin(45, 150, 0, 0)
@@ -56,51 +50,46 @@ win.keypad(1)
 curses.noecho()
 curses.curs_set(0)
 win.border(0)
-
-# window.border([ls[, rs[, ts[, bs[, tl[, tr[, bl[, br]]]]]]]])
 win.nodelay(1)
 box.nodelay(1)
 
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)         # def colours
+curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)        # def colours
+curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)           # def colours
+curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_YELLOW)        # def colours
 
-# colors definte
-# def color of snake
-curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)
-# def colours
-curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_YELLOW)
-# set color of bgs
-win.bkgd(" ", curses.color_pair(2))
-box.bkgd(" ", curses.color_pair(3))
+win.bkgd(" ", curses.color_pair(2))                                 # set color of WINDOW
+box.bkgd(" ", curses.color_pair(3))                                 # set color of BOX
 box.refresh()
 score = 0
-# Initializing values
+
+"""INITIALIZING VALUES"""
 key = KEY_RIGHT
 timer = 0
 timer2 = 0
-# stuff of game
-# Initial snake co-ordinates
-snake = [[4, 10], [4, 9], [4, 8]]
-# First food co-ordinates
-food1 = [15, 20]
-food2 = [30, 50]
-# print
-# Prints the food1
-win.addch(food1[0], food1[1], '*')
-# Prints the food2
-win.addch(food2[0], food2[1], '*')
-# thegameprocess
+x = randint(60, 80)
+y = randint(10, 20)
+
+"""DEFINING SNAKE AND FOOD"""
+snake = [[4, 10], [4, 9], [4, 8]]  # Initial snake co-ordinates
+food1 = [15, 20]  # First food co-ordinates
+food2 = [30, 50]  # First food co-ordinates
+
+win.addch(food1[0], food1[1], '*')  # Prints the food1
+win.addch(food2[0], food2[1], '*')  # Prints the food2
+
+
+""" GAME PROCESS"""
 while key != 27:
-    box.border(0)                                                  # While Esc key is not pressed
+    box.border(0)                                                 # While Esc key is not pressed
     win.border(0)
-    # Printing 'Score' and
-    win.addstr(0, 10, 'Score : ' + str(score) + ' ')
-    win.addstr(0, 40, 'Time : ' + str(timer2) + ' ')
-    # 'SNAKE' strings
-    win.addstr(0, 70, ' SNAKE ')
-    # Increases the speed of Snake as its length increases
-    win.timeout(speed)
-    # Previous key pressed
+
+    win.addstr(0, 10, 'Score : ' + str(score) + ' ')              # Printing 'Score'
+    win.addstr(0, 125, 'Time : ' + str(timer2) + ' ')             # Printing 'Timer'
+    win.addstr(0, 70, ' SNAKE ')                                  # Printing 'SNAKE'
+    win.timeout(speed)                                            # Increases the speed of Snake as its length increases
+
+    """ Previous key pressed"""
     prevKey = key
     event = win.getch()
     if event == KEY_DOWN and prevKey == KEY_UP:
@@ -115,23 +104,22 @@ while key != 27:
         key = key
     else:
         key = event
-    # Pause
-    # If SPACE BAR is pressed, wait for another
+
+    """ Pause"""
     if key == ord(' '):
-        # one (Pause/Resume)
         key = -1
         while key != ord(' '):
             key = win.getch()
         key = prevKey
         continue
-    # if the user is stupid
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
+
+    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:               # If an invalid key is pressed
         key = prevKey
     # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
     # This is taken care of later at [1].
     snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1),
                      snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
-    # If snake crosses the boundaries, make it enter from the other side
+    """ If snake crosses the boundaries, make it enter from the other side"""
     if mode == 1:
         if snake[0][0] == 0:
             snake[0][0] = 41
@@ -141,7 +129,8 @@ while key != 27:
             snake[0][0] = 1
         if snake[0][1] == 145:
             snake[0][1] = 1
-    # Exit if snake crosses the boundaries
+
+    """ Exit if snake crosses the boundaries"""
     if mode == 2:
         if snake[0][0] == 0 or snake[0][0] == 44 or snake[0][1] == 0 or snake[0][1] == 149:
             GameOver()
@@ -152,7 +141,7 @@ while key != 27:
     if score == 10:
         appears_wall_horizontal(y)
 
-    # Events of gameover
+    """ Events of gameover"""
     if snake[0] in snake[1:]:
         GameOver()
         break
@@ -165,7 +154,8 @@ while key != 27:
         GameOver()
         break
 
-    if snake[0] == food1:                                            # When snake eats the food
+    """ When snake eats the food1"""
+    if snake[0] == food1:
         food1 = []
         score += 1
         while food1 == []:
@@ -178,7 +168,8 @@ while key != 27:
             if food1[0] <= 40 and food1[1] == x:
                 food1 = []
         win.addch(food1[0], food1[1], '*')
-    # When snake eats the food
+
+    """ When snake eats the food2"""
     elif snake[0] == food2:
         food2 = []
         score += 1
@@ -193,7 +184,7 @@ while key != 27:
                 food2 = []
         win.addch(food2[0], food2[1], '*')
     else:
-        # [1] If it does not eat the food, length decreases
+        # If it does not eat the food, length decreases
         last = snake.pop()
         win.addch(last[0], last[1], ' ')
     win.addch(snake[0][0], snake[0][1], 'x', curses.color_pair(3))
@@ -203,3 +194,5 @@ while key != 27:
 
 curses.endwin()
 print("\nScore:" + str(score))
+
+"""THE END"""
